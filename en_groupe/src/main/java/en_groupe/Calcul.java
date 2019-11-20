9f01a71c72330b06385a6cdf2445a9d19df1bb09
mbodjiousseynou <mbodjiousseynou@10.153.73.215>
2019-11-20 10:49:46 +0000

    notre premiere commit

---
+++ en_groupe/src/main/java/en_groupe/Calcul.java
@@ -0,0 +1,13 @@
+package en_groupe;
+
+public class Calcul {
+	int addition(int a,int b) {
+		int som= a+b;
+		return som;
+	}
+	int multiplication(int c,int d) {
+		int prod=c*d;
+		return prod;
+	}
+
+}
